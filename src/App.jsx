2fd1c56ec417fe1871b4a4c93609bc0a6709b271
Hank Kapka <hankkapka@gmail.com>
2022-12-18 11:03:09 -0600

    added endggame modal to display final scores in the future

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@
 import Navbar from './components/Navbar'
 import PlayerForm from './components/PlayerForm';
 import ScoreList from './components/ScoreList';
+import ScoreModal from './components/scoreModal';
 import styles from './style'
 
+
 import React, { useState, createContext } from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 export const PageContext = createContext(true);
 
@@ -13,20 +17,38 @@ function App() {
   // const [currentPage, setCurrentPage] = useState('PlayerForm');
   const [isToggled, setIsToggled] = useState(true);
 
+  //score modal
+  const [modalOn, setModalOn] = useState(false);
+  const [choice, setChoice] = useState(false)
+
+  const clicked = () => {
+    setModalOn(true)
+  }
+
   return (
-    <div className="bg-primary w-full overflow-hidden h-screen">
+
+    <div className="bg-primary w-full overflow-visible h-screen">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
+
         </div>
       </div>
       <div className={`bg-primary mb-auto ${styles.flexStart}`} >
-        <div className="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">
+        <div className="py-10 px-10 mx-0 min-w-full flex flex-col items-center">
           <PageContext.Provider value={{ isToggled, setIsToggled }}>
             {isToggled ? <PlayerForm name={name} setName={setName} /> : <ScoreList name={name} setName={setName} />}
 
             {isToggled ? <button className="bg-white text-black hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded" onClick={() => setIsToggled(!isToggled)}>Start</button> :
-              <><button className="bg-white text-black hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded" onClick={() => (console.log("button"))}>End Game</button>
+              <>
+                {/* conditionally render the score modal when the endgame button is clicked */}
+                {modalOn && < ScoreModal setModalOn={setModalOn} setChoice={setChoice} />}
+
+                <div to="/Disc-Golf-Skins-ScoreKeeper/EndGame"
+                  className="bg-white text-black hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded"
+                  onClick={clicked}> End Game</div>
+
+
                 <button className="bg-white text-black hover:bg-blue-400 font-bold py-2 px-4 mt-3 rounded" onClick={() => setIsToggled(!isToggled)}>New Game</button> </>}
           </PageContext.Provider>
         </div>
--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -19,21 +19,27 @@ function PlayerForm({ name, setName }) {
     return (
         <form onSubmit={addPlayer}>
             <div className='w-full flex py-6
-        justify-between items-center navbar
-        font-poppins font-normal text-[16px]
-        text-white'>
-                <div><h1 className="py-6 text-[36px]"> Add Players: </h1>
-                    <input className='submitButton text-black p-2 h-12 rounded'
+                            justify-between items-center navbar
+                            font-poppins font-normal text-[16px]
+                            text-white'>
+                <div className="grid grid-cols-3 gap-4">
+                    <h1 className="py-6 text-[36px] col-span-3"> Add Players: </h1>
+                    <input className='submitButton text-black p-2 h-12 rounded col-span-2'
                         type='text'
                         name="playerName"
                         id="playerName"
                         placeholder="Player Name..."
                         ref={playerName} />
-                    <input className='bg-white text-black hover:bg-blue-400 font-bold m-2 py-1 px-3 mt-1 rounded' type='submit' value='+'></input>
+                    <input className='  bg-white text-black 
+                                        hover:bg-blue-400 font-bold m-2 
+                                        py-1 px-3 mt-1 rounded' type='submit' value='+'></input>
+
+
                     <div>
-                        <ul className="list-none flex  flex-col justify-end
-            items-center text-[24px]
-            py-6">
+                        <ul className=" list-none flex  
+                                        flex-col justify-end
+                                        items-center text-[24px]
+                                        py-6">
                             <PlayerList name={name} setName={setName} />
                         </ul>
                     </div>
